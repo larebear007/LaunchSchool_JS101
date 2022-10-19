152fd2fe4fc18252577c27bfc7ef46bb88caf935
larebear007 <larencozart@gmail.com>
2022-10-19 13:53:34 -0500

    Initial commit

---
+++ Lesson2/pseudo_code_practice.js
@@ -0,0 +1,35 @@
+// ex 1 : function that returns the sum of two numbers
+
+    // START
+    // DEFINE function name
+    // SET 2 parameters, 'num1' 'num2'
+    // RETURN parameter 1 + parameter 2
+    // PRINT value of called function
+    // END
+
+// ex 2: function that takes an array of strings, 
+// and returns a string that is all those strings concatenated together
+
+    // START
+    // DEFINE function name
+    // SET parameter as 'arrayOfStrings'
+    // SET variable for concatenated string = array.methodToJoinStrings('')
+    // RETURN string variable
+    // PRINT value of called function
+    // END
+
+// ex 3: a method that takes an array of integers, 
+// and returns a new array with every other element 
+// from the original array, starting with the first element
+
+    // START
+    // DEFINE function name
+    // SET parameter as 'arrayOfInts'
+    // SET index variable = 0
+    // SET new array = []
+    // WHILE index < length of the array
+        // put array[index value] into new array 
+        // SET index += 2
+    // RETURN new array
+    // PRINT value of called function
+    // END
