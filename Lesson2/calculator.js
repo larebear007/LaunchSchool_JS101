749f14467784a11331f6d160efe9491e6a6caba9
larebear007 <larencozart@gmail.com>
2022-10-24 09:15:41 -0500

    Added features: invalid input check and distinctive prompt.

--- Lesson2/calculator.js
+++ Lesson2/calculator.js
@@ -4,26 +4,56 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 
-console.log('Welcome to Calculator!');
-
 const rlsync = require('readline-sync');
-
-let numOne = rlsync.question('What\'s the first number?  ');
-let numTwo = rlsync.question('What\'s the second number?  ');
-let operation = rlsync.question('What operation would you like to perform?\n\
-1 - Add\n2 - Subtract\n3 - Multiply\n4 - Divide\n');
-
-let answer;
-if (operation === '1') {
-  answer = Number(numOne) + Number(numTwo);
-} else if (operation === '2') {
-  answer = Number(numOne) - Number(numTwo);
-} else if (operation === '3') {
-  answer = Number(numOne) * Number(numTwo);
-} else if (operation === '4') {
-  answer = Number(numOne) / Number(numTwo);
-} else {
-  console.log('I didn\'t understand your numbers or operation. Try that again.');
+let prompt = (message) => console.log(`=> ${message}`);
+let invalidNumber = (num) => {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}  
+let invalidOperation = (operation) => {
+  let validOperations = [1, 2, 3, 4];
+  return !validOperations.includes(Number(operation));
 }
 
-console.log(`The result is ${answer}.`);
+prompt('Welcome to Calculator!');
+
+let numOne = rlsync.question(prompt('What\'s the first number?  '));
+while (invalidNumber(numOne)) {
+  prompt('Please enter a valid number.');
+  numOne = rlsync.question(prompt('What\'s the first number?  '));
+}
+
+let numTwo = rlsync.question(prompt('What\'s the second number?  '));
+while (invalidNumber(numTwo)) {
+  prompt('Please enter a valid number.');
+  numTwo = rlsync.question(prompt('What\'s the second number?  '));
+}
+
+let operation = rlsync.question(prompt('What operation would you like to perform?\n\
+1 - Add\n2 - Subtract\n3 - Multiply\n4 - Divide\n'));
+while (invalidOperation(operation)) {
+  prompt('Please enter a valid operation option: 1, 2, 3, or 4.');
+  operation = rlsync.question(prompt('What operation would you like to perform?\n\
+  1 - Add\n2 - Subtract\n3 - Multiply\n4 - Divide\n'));
+}
+
+
+let answer;
+switch (operation) {
+  case '1':
+    answer = Number(numOne) + Number(numTwo);
+    break;
+  case '2':
+    answer = Number(numOne) - Number(numTwo);
+    break;
+  case '3':
+    answer = Number(numOne) * Number(numTwo);
+    break;
+  case '4':
+    answer = Number(numOne) / Number(numTwo);
+    break;
+  default:
+    prompt('I didn\'t understand your numbers or operation. Try that again.')
+}
+
+
+prompt(`The result is ${answer}.`);
