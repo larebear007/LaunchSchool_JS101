1cbc413a2216d5e8a4a27461fee989b74e8d00cf
larebear007 <larencozart@gmail.com>
2022-10-27 08:20:33 -0500

    Linted & removed pseudocode

--- Lesson2/loan_calculator.js
+++ Lesson2/loan_calculator.js
@@ -1,34 +1,28 @@
-// Dependecies
 const RLSYNC = require('readline-sync');
 const MESSAGES = require('./loan_calc_messages.json');
 const LANG = RLSYNC.question(MESSAGES['language']);
-let prompt = (message, extraMessage='') => {
+let prompt = (message, extraMessage = '') => {
   if (LANG === 'en') {
     console.log(`> ${MESSAGES['en'][message]}` + extraMessage);
   } else if (LANG === 'es') {
     console.log(`> ${MESSAGES['es'][message]}` + extraMessage);
   }
-}
+};
 
 let repeat = true;
 while (repeat) {
   prompt('welcome');
 
-  // INFORMATION
-  // 1. loan amount
   let loanAmount = RLSYNC.question(prompt('loanAmount'));
   loanAmount = Number(loanAmount);
-  // check valid input (without commas or extra chars)
   while (Number.isNaN(loanAmount)) {
     prompt('invalidLoanAmount');
     loanAmount = RLSYNC.question(prompt('loanAmount'));
     loanAmount = Number(loanAmount);
   }
 
-  // 2. apr -> monthly
-  let annualIR = RLSYNC.question(prompt('apr'))
+  let annualIR = RLSYNC.question(prompt('apr'));
   annualIR = Number(annualIR);
-  // check for invalid input
   while (Number.isNaN(annualIR)) {
     prompt('invalidAPR');
     annualIR = RLSYNC.question(prompt('apr'));
@@ -37,9 +31,7 @@ while (repeat) {
   let monthlyIR = (annualIR / 100) / 12;
   console.log(`**monthlyIR = ${monthlyIR}`);
 
-  // 3. loan duration -> months
   let yearsOrMonths = RLSYNC.question(prompt('yearsOrMonths'));
-  // check for invalid input
   let validYearsOrMonths = ['years', 'months', 'anos', 'años', 'meses'];
   while (!validYearsOrMonths.includes(yearsOrMonths)) {
     prompt('invalidYearsOrMonths');
@@ -47,7 +39,6 @@ while (repeat) {
   }
   let loanDuration = RLSYNC.question(prompt('loanDuration', yearsOrMonths + '.'));
   loanDuration = Number(loanDuration);
-  // check for invalid input
   while (Number.isNaN(loanDuration)) {
     prompt('invalidLoanDuration');
     loanDuration = RLSYNC.question(prompt('loanDuration', yearsOrMonths + '.'));
@@ -55,14 +46,12 @@ while (repeat) {
   }
   let years = ['years', 'anos', 'años'];
   if (years.includes(yearsOrMonths)) {
-    loanDuration = loanDuration * 12;
+    loanDuration *= 12;
   }
 
-  // CALCULATION
   let monthlyPayment = loanAmount * (monthlyIR / (1 - Math.pow((1 + monthlyIR), (-loanDuration))));
   prompt('monthlyPayment', '$' + monthlyPayment.toFixed(2) + '.');
 
-  // REPEAT
   let repeatQuestion = RLSYNC.question(prompt('repeat'));
   switch (LANG) {
     case 'en':
