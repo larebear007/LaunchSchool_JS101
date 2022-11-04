1ea1a23ddb69012f5d663b2034feaeb692fadb28
larebear007 <larencozart@gmail.com>
2022-11-04 09:18:25 -0500

    Add features: helper functions, single char input, best 3 out of 5, lizard spock choices.

--- Lesson2/rock_paper_scissors.js
+++ Lesson2/rock_paper_scissors.js
@@ -3,26 +3,25 @@ const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 const RLSYNC = require('readline-sync');
-const VALID_CHOICES = ['rock', 'r', 'paper', 'p', 'scissors', 'scissor', 'sc', 'lizard', 'l', 'spock', 'sp'];
+const VALID_USER_CHOICES = ['rock', 'r', 'paper', 'p', 'scissors', 'scissor', 'sc', 'lizard', 'l', 'spock', 'sp'];
+const VALID_COMPUTER_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const WINNING_COMBOS = {
-  rock : ['scissors', 'scissor', 'sc', 'lizard', 'l'] ,
-  r : ['scissors', 'scissor', 'sc', 'lizard', 'l'] ,
-  paper : ['rock', 'r', 'spock', 'sp'] ,
-  p : ['rock', 'r', 'spock', 'sp'] ,
-  scissors : ['paper', 'p', 'lizard', 'l'] ,
-  sc : ['paper', 'p', 'lizard', 'l'] ,
-  lizard : ['spock', 'sp', 'paper', 'p'] ,
-  l : ['spock', 'sp', 'paper', 'p'] ,
-  spock : ['scissors', 'scissor', 'sc', 'rock', 'r'] ,
-  sp : ['scissors', 'scissor', 'sc', 'rock', 'r']
+  rock : ['scissors', 'lizard',] ,
+  paper : ['rock', 'spock'] ,
+  scissors : ['paper', 'lizard'] ,
+  lizard : ['spock', 'paper'] ,
+  spock : ['scissors', 'rock'] ,
+
 }
+let USER_WINS;
+let COMPUTER_WINS;
+
+
 
 async function displayWelcome() {
-  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!');
+  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!\n');
   await sleep(500);
-  prompt('Best 3 out of 5 wins the game.');
-  await sleep(500);
-  prompt('Let\'s begin!\n\n');
+  prompt('Best 3 out of 5 wins the game.\n');
   await sleep(500);
 
   prompt('While playing, choose one of the following actions:');
@@ -35,83 +34,118 @@ async function displayWelcome() {
   await sleep(500);
   prompt('lizard   or   l');
   await sleep(500);
-  prompt('spock    or   sp:  ');
+  prompt('spock    or   sp:\n');
+  await sleep(500);
+
+  prompt('Let\'s begin!');
 }  
+
+function mapUserInput(input) {
+
+  if (input === 'r') {
+    input = 'rock';
+  } else if (input === 'p') {
+    input = 'paper';
+  } else if (input === 'sc' || input === 'scissor') {
+    input = 'scissors';
+  } else if (input === 'l') {
+    input = 'lizard';
+  } else if (input === 'sp') {
+    input = 'spock';
+  }
+
+  return input;
+}
 
 function getUserChoice() {
   let userChoice = RLSYNC.question(prompt('Your choice: ')).toLowerCase();
+  userChoice = mapUserInput(userChoice);
+  console.log(`***** ${userChoice}`); ///
 
-  while (!VALID_CHOICES.includes(userChoice)) {
+  while (!VALID_USER_CHOICES.includes(userChoice)) {
     prompt('Invalid choice.');
-    userChoice = RLSYNC.question(prompt('Choose one aciton: rock, paper, scissor'))
+    userChoice = RLSYNC.question(prompt('Choose one aciton: rock, paper, scissors, lizard, spock'))
                 .toLowerCase();
+    userChoice = mapUserInput(userChoice);
+    console.log(`***** ${userChoice}`);
   }
+
   return userChoice;
 }
 
 function getComputerChoice() {  
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  let randomIndex = Math.floor(Math.random() * VALID_COMPUTER_CHOICES.length);
+  let computerChoice = VALID_COMPUTER_CHOICES[randomIndex];
 
   return computerChoice;
 }
 
 function displayRounds() {
-  let gameRound = 1;
-  let userWins = 0;
-  let computerWins = 0;
-  let score = `--ROUND ${gameRound} SCORE-- You: ${userWins}, Computer:${computerWins}`;
+  let gameRound = 0;
+  let score;
+  USER_WINS = 0;
+  COMPUTER_WINS = 0;
+  
   while (gameRound <= 5) {
-    if (userWins === 3 || computerWins === 3) {
+    if (USER_WINS === 3 || COMPUTER_WINS === 3) {
       break;
-    } else if (WINNING_COMBOS[getUserChoice()].includes(getComputerChoice())) {
-      userWins += 1;
-      prompt('Woohoo! You won!\n' + score);
-    } else if (getComputerChoice() === getComputerChoice()) {
-      prompt('Tie!\n' + score);
-    } else {
-      computerWins += 1;
-      prompt('Ah rats! Computer won.\n' + score);
     }
 
-    gameRound += 1;
+    let currentUserChoice = getUserChoice();
+    let currentComputerChoice = getComputerChoice();
+    prompt(`Computer choice: ${currentComputerChoice}`);
+
+    if (WINNING_COMBOS[currentUserChoice].includes(currentComputerChoice)) {
+      USER_WINS += 1;
+      gameRound += 1;
+      prompt('You won!');
+    } else if (currentUserChoice === currentComputerChoice) {
+      prompt('Tie! Let\'s try that again.');
+    } else {
+      COMPUTER_WINS += 1;
+      gameRound += 1;
+      prompt('Computer won.');
+    }
+
+    score = `ROUND ${gameRound} SCORE -- You: ${USER_WINS}, Computer: ${COMPUTER_WINS}\n`;
+    prompt(score);
   }
 }
 
 function displayWinner() {
-  if (userWins > computerWins) {
-    prompt('FINAL RESULT:\n\
-    CONGRATULATIONS! You beat the computer at Rock, Paper, Scissors, Lizard, Spock!');
-  } else if (computerWins > userWins) {
-    prompt('FINAL RESULT:\n\
-    Better luck next time. Computer beat you at Rock, Paper, Scissors, Lizard, Spock!');
+  if (USER_WINS > COMPUTER_WINS) {
+    prompt('FINAL RESULT:\nCONGRATULATIONS! You beat the computer at Rock, Paper, Scissors, Lizard, Spock!\n');
+
+  } else if (COMPUTER_WINS > USER_WINS) {
+    prompt('FINAL RESULT:\nBetter luck next time. Computer beat you at Rock, Paper, Scissors, Lizard, Spock!\n');
+
   } else {
-    console.log('*** ERROR *** within userWins computerWins totals');
+    console.log('*** ERROR *** within displayWinner() and displayRounds()');
   }
 }
 
-function playGame() {    
+function playAgain() {    
     while (true) {
-      displayRounds();
-      displayWinner();
-
-      let playAgain = RLSYNC.question(prompt('Would you like to play again?\n\
-      Please respond with "yes" or "no":  ')).toLowerCase();
-      if (playAgain === 'yes' || playAgain) {
-        continue;
+      let playAgain = RLSYNC.question(prompt('Would you like to play again?\nPlease respond with "yes" or "no":  ')).toLowerCase();
+      if (playAgain === 'yes' || playAgain === 'y') {
+        break;
       } else if (playAgain === 'no' || playAgain === 'n') {
         prompt('Thanks for playing Rock, Paper, Scissors, Lizard, Spock! Goodbye!');
-        break;
+        return;
       } else {
         prompt('Invalid input: Restart the program to play again.');
-        break;
+        return;
       }
     }
+  
+    game();
 }
 
 async function game() {
   await displayWelcome();
-  playGame();
+  displayRounds();
+  displayWinner();
+  playAgain();
 }
 
 game();
