da031c0bfbf7443a8e32b6e88c3c39ea5e80cd2a
larebear007 <larencozart@gmail.com>
2022-11-13 19:44:00 -0600

    Completed Q8

--- Small_Problems/easy2.js
+++ Small_Problems/easy2.js
@@ -51,3 +51,63 @@ console.log(square(0));
 console.log(square(1));
 console.log(square(2));
 console.log(square(5));
+
+// 5. Arithmetic Integer
+console.log('\n-- Question 5 --');
+
+let displayOperations = () => {
+  let rlsync = require('readline-sync');
+  let num1 = parseInt(rlsync.question('Enter the first integer: '));
+  let num2 = parseInt(rlsync.question('Enter the second integer: '));
+
+  console.log(`Addition: ${num1} + ${num2} = ${num1 + num2}`);
+  console.log(`Subtraction: ${num1} - ${num2} = ${num1 - num2}`);
+  console.log(`Product: ${num1} * ${num2} = ${num1 * num2}`);
+  console.log(`Quotient: ${num1} / ${num2} = ${num1 / num2}`);
+  console.log(`Remainder: ${num1} % ${num2} = ${num1 % num2}`);
+  console.log(`Power: ${num1} ** ${num2} = ${num1 ** num2}`);
+}
+
+displayOperations();
+
+// 6. The End is Near But Not Here
+console.log('\n-- Question 6 --');
+
+let penultimate = (text) => {
+  let words = text.split(' ');
+  return words[words.length - 2];
+}
+
+console.log(penultimate('Accentos pueden estar en la uúltima, penúltima, y esdrújula sílaba.'));
+console.log(penultimate('Ultimate penultimate antepenultimate'));
+console.log(penultimate('Hola mundo'));
+
+// 7. Exclusive Or
+console.log('\n-- Question 7 --');
+
+let xor = (arg1, arg2) => {
+  if (arg1 && arg2) {
+    return false;
+  } else if (arg1 || arg2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(xor(0, 1)); // true
+console.log(xor(true, false)); // true
+console.log(xor('', [])); // true
+console.log(xor(undefined, null)); // false
+console.log(xor('hello', 10)); // false 
+
+// 8. Odd Lists
+console.log('\n-- Question 8 --');
+
+let oddLists = (array) => {
+  return array.filter( (element) => array.indexOf(element) % 2 === 1);
+}
+
+console.log(oddLists([1, 2, 3, 4, 5]));
+console.log(oddLists([0, -1 , 0, -1, 0])); 
+console.log(oddLists(['odd', 'even', 'odd', 'even']));
