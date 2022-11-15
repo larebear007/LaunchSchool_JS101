202886e5747d0a7dc2f68b32fc260a6d56049377
larebear007 <larencozart@gmail.com>
2022-11-14 21:18:06 -0600

    Completed 4/10 problems

--- Small_Problems/easy3.js
+++ Small_Problems/easy3.js
@@ -40,35 +40,70 @@ bannerizer('Happy Thanksgiving!');
 console.log('\n-- Question 3--');
 
 let stringy = (number) => {
-  if (number < 1) return '';
-  
   let array = [];
   array.length = number;
+  if (number < 1) return array.join('');
 
-  for (i = 0; i < array.length; i += 1) {
-    if (i === 0 || i % 2 == 0) {
-      array[i] = 1;
-    } else if(i % 2 == 1) {
-      array[i] == 0;
+  for (idx = 0; idx < number; idx += 1) {
+    if (idx % 2 === 0) {
+      array[idx] = 1;
+    } else if (idx % 2 === 1) {
+      array[idx] = 0;
     }
-    
   }
-  console.log(array);
+  
   return array.join('');
 }
 
-console.log(stringy(-1));
 console.log(stringy(0));
 console.log(stringy(1));
 console.log(stringy(2));
 console.log(stringy(10));
 
-
-
-// 4. 
+// 4. Fibonacci Number Location By Length
 console.log('\n-- Question 4--');
 
-// 5. 
+let memo = {};
+
+let fibonacci = (num) => {
+  if (memo[num]) return memo[num];
+  if (num === 1n || num === 2n) {
+    return 1n;
+  }
+
+  return memo[num] = fibonacci(num - 1n) + fibonacci(num - 2n);
+}
+
+let findFibonacciIndexByDigits = (digits) => {
+
+  let fibonacciArray = [0];
+  let fibNum;
+
+  let num = 1n;
+  while (true) {
+    fibNum = fibonacci(num);
+    fibonacciArray.push(fibNum);
+    
+    if (fibNum >= (10n ** (digits - 1n))) {
+      return fibonacciArray.indexOf(fibNum);
+    }
+
+    num += 1n;
+  }
+
+}
+
+
+console.log(findFibonacciIndexByDigits(2n)) //=== 7n;    // 1 1 2 3 5 8 13
+console.log(findFibonacciIndexByDigits(3n)) //=== 12n;   // 1 1 2 3 5 8 13 21 34 55 89 144
+console.log(findFibonacciIndexByDigits(10n)) //=== 45n;
+console.log(findFibonacciIndexByDigits(16n)) //=== 74n;
+console.log(findFibonacciIndexByDigits(100n)) //=== 476n;
+console.log(findFibonacciIndexByDigits(1000n)) //=== 4782n;
+console.log(findFibonacciIndexByDigits(10000n)) //=== 47847n;
+
+
+// 5. Right Triangles
 console.log('\n-- Question 5--');
 
 // 6. 
