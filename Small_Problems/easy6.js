cd9ae5df2d511059222f208fe7c94091b0d5885a
larebear007 <larencozart@gmail.com>
2022-11-28 09:54:37 -0600

    Added setup

---
+++ Small_Problems/easy6.js
@@ -0,0 +1,35 @@
+// 1. 
+console.log('-- Question 1 --');
+
+// 2.
+console.log('\n-- Question 2 --');
+
+// 3.
+console.log('\n-- Question 3 --');
+
+// 4.
+console.log('\n-- Question 4 --');
+
+// 5.
+console.log('\n-- Question 5 --');
+
+// 6.
+console.log('\n-- Question 6 --');
+
+// 7.
+console.log('\n-- Question 7 --');
+
+// 8.
+console.log('\n-- Question 8 --');
+
+// 9.
+console.log('\n-- Question 9 --');
+
+// 10.
+console.log('\n-- Question 10 --');
+
+// 11.
+console.log('\n-- Question 11 --');
+
+// 12.
+console.log('\n-- Question 12 --');
