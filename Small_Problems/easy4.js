393b2d7a539eca98065c96349924a0bbdbb38fbc
larebear007 <larencozart@gmail.com>
2022-11-29 10:30:32 -0600

    Completed all problems

--- Small_Problems/easy4.js
+++ Small_Problems/easy4.js
@@ -60,20 +60,116 @@ console.log(isPalindrome('Madam'));
 console.log(isPalindrome("madam i'm adam"));
 console.log(isPalindrome('356653'));
 
-// 5.
+// 5. Palindromic Strings (part 2)
 console.log('\n-- Question 5 --');
 
-// 6.
+let isRealPalindrome = (string) => {
+  const alphaNumericChars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+  let array = [];
+  
+  for (i = 0; i < string.length; i += 1) {
+    if (alphaNumericChars.includes(string[i].toLowerCase())) {
+      array.push(string[i].toLowerCase());
+    }
+  }
+  console.log(array.join('') + ' : ' + array.reverse().join(''));
+  return array.join('') === array.reverse().join('');
+}
+
+console.log(isRealPalindrome('madam'));               // true
+console.log(isRealPalindrome('Madam'));               // true (case does not matter)
+console.log(isRealPalindrome("Madam, I'm Adam"));     // true (only alphanumerics matter)
+console.log(isRealPalindrome('356653'));              // true
+console.log(isRealPalindrome('356a653'));             // true
+console.log(isRealPalindrome('123ab321'));            // false
+
+// 6. Palindromic Numbers
 console.log('\n-- Question 6 --');
 
-// 7.
+let isPalindromicNumber = (num) => {
+  return String(num) === String(num).split('').reverse().join('');
+}
+
+console.log(isPalindromicNumber(34543));
+console.log(isPalindromicNumber(123210));
+console.log(isPalindromicNumber(22));
+console.log(isPalindromicNumber(5));
+
+// 7. Running Totals
 console.log('\n-- Question 7 --');
 
-// 8.
+let runningTotal = (array) => {
+  let total = 0;
+  let runningTotalArray = array.map((num) => total += num);
+
+  return runningTotalArray;
+}
+
+console.log(runningTotal([2, 5, 13]));
+console.log(runningTotal([14, 11, 7, 15, 20]));
+console.log(runningTotal([3]));
+console.log(runningTotal([]));
+
+// 8. Letter Counter (part 1)
 console.log('\n-- Question 8 --');
 
-// 9.
+let wordSizes = (text) => {
+  let words = text.split(' ');
+  let object = {};
+  if (text.trim() !== '') {
+    words.forEach(word => {
+      key = word.length;
+      object.hasOwnProperty(key) ? object[key] += 1 : object[key] = 1;
+    })
+  }
+  return object;
+}
+
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
+console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes(''));                                            // {}
+
+// 9. Letter Counter (part 2)
 console.log('\n-- Question 9 --');
 
-// 10.
+let cleanUpWord = (word) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let cleanWord = word.split('').filter(char => alphabet.includes(char));
+  return cleanWord.join('');
+}
+
+let realWordSizes = (text) => {
+  let words = text.split(' ');
+  let object = {};
+  if (text.trim() !== '') {
+    words.forEach(word => {
+      word = cleanUpWord(word);
+      key = word.length;
+      object.hasOwnProperty(key) ? object[key] += 1 : object[key] = 1;
+    })
+  }
+  return object;
+}
+
+console.log(realWordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 2 }
+console.log(realWordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 3 }
+console.log(realWordSizes("What's up doc?"));                              // { "2": 1, "3": 1, "5": 1 }
+console.log(realWordSizes(''));                                            // {}
+
+// 10. Letter Swap
 console.log('\n-- Question 10 --');
+
+let swap = (text) => {
+  if (text.length === 1) return text;
+  let words = text.split(' ');
+  let swappedWords = words.map(word => {
+    return word.slice(-1) + word.slice(1, -1) + word.slice(0, 1);
+  })
+
+  return swappedWords.join(' ');
+}
+
+console.log(swap('Oh what a wonderful day it is'));  // "hO thaw a londerfuw yad ti si"
+console.log(swap('Abcde'));                          // "ebcdA"
+console.log(swap('a'));        
