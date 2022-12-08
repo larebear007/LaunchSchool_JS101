70a66f106dad0155dde50c915fec45dc95ac4611
larebear007 <larencozart@gmail.com>
2022-12-08 09:58:50 -0600

    Completed 5/10 problems

--- Small_Problems/string_text_processing.js
+++ Small_Problems/string_text_processing.js
@@ -1,28 +1,96 @@
-// 1. 
+// 1. UpperCase Check
 console.log('-- Question 1 --');
 
+let isUpperCase = (string) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let alphCharsInString = string.split('').filter(char => alphabet.includes(char));
+  return alphCharsInString.every(char => char === char.toUpperCase());
+}
 
-// 2. 
+console.log(isUpperCase('t'));
+console.log(isUpperCase('T'));
+console.log(isUpperCase('Four Score'));
+console.log(isUpperCase('FOUR SCORE'));
+console.log(isUpperCase('4SCORE!'));
+console.log(isUpperCase(''));
+
+
+// 2. Delete Vowels
 console.log('\n-- Question 2 --');
 
+let removeVowels = (array) => {
+  const VOWELS = 'aeiouAEIOU';
+  return array.map(string => string.split('').filter(char => !VOWELS.includes(char)).join(''));
+}
 
-// 3. 
+console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));
+console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));
+console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));
+
+// 3. LetterCase Counter
 console.log('\n-- Question 3 --');
 
+let letterCaseCount = (string) => {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let obj = { lowercase: 0, uppercase: 0, neither : 0 };
+  string.split('').forEach(char => {
+    if (alphabet.includes(char)) {
+      if (char === char.toLowerCase()) {
+        obj.lowercase += 1;
+      } else if (char === char.toUpperCase()) {
+        obj.uppercase += 1;
+      }
+    } else {
+      obj.neither += 1;
+    }
+  });
+  return obj;
+}
 
-// 4.
+console.log(letterCaseCount('abCdef 123'));
+console.log(letterCaseCount('AbCd +Ef'));
+console.log(letterCaseCount('123'));
+console.log(letterCaseCount(''));
+
+// 4. Capitalize Words
 console.log('\n-- Question 4 --');
 
+let wordCap = (string) => {
+  return string
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join(' ');
+}
 
-// 5.
+console.log(wordCap('four score and seven'));
+console.log(wordCap('the javaScript language'));
+console.log(wordCap('this is a "quoted" word'));
+
+// 5. Swap Case
 console.log('\n-- Question 5 --');
 
+let swapCase = (string) => {
+  return string
+    .split('')
+    .map(char => {
+      if (char === char.toLowerCase()) return char.toUpperCase();
+      else if (char === char.toUpperCase()) return char.toLowerCase();
+      else return char;
+    })
+    .join('');
+}
 
-// 6.
+console.log(swapCase('CamelCase'));
+console.log(swapCase('Tonight on XYZ-TV'));
+
+
+// 6. Staggered Caps (part 1)
 console.log('\n-- Question 6 --');
 
 
-// 7.
+
+
+// 7. Staggered Caps (part 2)
 console.log('\n-- Question 7 --');
 
 
@@ -38,9 +106,3 @@ console.log('\n-- Question 9 --');
 console.log('\n-- Question 10 --');
 
 
-// 11. 
-console.log('\n-- Question 11 --');
-
-
-// 12. 
-console.log('\n-- Question 12 --');
